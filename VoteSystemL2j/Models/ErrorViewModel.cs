d0a5ca38b8ae6c8ea6719fc785dc930c44ab2a83
Richard <Richard@DESKTOP-9LM3OJE>
2020-04-04 03:16:37 -0300

    Adicionar arquivos de projeto.

---
+++ VoteSystemL2j/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VoteSystemL2j.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
